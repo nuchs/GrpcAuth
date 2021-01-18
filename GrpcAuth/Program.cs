39eb1a893f63a8be5c93a8ba3dc03df7bb554def
nuchs <sjbrown@live.co.uk>
2021-01-18 14:31:20 +0000

    Server authentication up and running

--- GrpcAuth/Program.cs
+++ GrpcAuth/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace GrpcAuth
@@ -18,10 +19,18 @@ namespace GrpcAuth
         // Additional configuration is required to successfully run gRPC on macOS.
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
+
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.ConfigureHttpsDefaults(listenOptions =>
+                        {
+                            listenOptions.ServerCertificate = new X509Certificate2(@"Certs\grpc-server.pfx");
+                        });
+                    });
                 });
     }
 }
